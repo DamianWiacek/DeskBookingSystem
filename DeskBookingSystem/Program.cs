5b1e31201b8d470e0f41d6d2a1ab126c173ff125
DamianWiacek <damwiac0262@gmail.com>
2023-03-06 01:10:35 +0100

    DI update

--- DeskBookingSystem/Program.cs
+++ DeskBookingSystem/Program.cs
@@ -3,6 +3,7 @@ using DeskBookingSystem.Entities;
 using DeskBookingSystem.Middleware;
 using DeskBookingSystem.Models;
 using DeskBookingSystem.Models.Validators;
+using DeskBookingSystem.Repositories;
 using DeskBookingSystem.Services;
 using FluentValidation;
 using Microsoft.AspNetCore.Identity;
@@ -14,9 +15,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 var authenticationSettings = new AuthenticationSettings();
 builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
-builder.Services.AddSingleton(authenticationSettings);
-// Add services to the container.
 
+
+//Authentication configuration
 builder.Services.AddAuthentication(option =>
 {
     option.DefaultAuthenticateScheme = "Bearer";
@@ -33,6 +34,8 @@ builder.Services.AddAuthentication(option =>
     };
 });
 
+// Add services to the container.
+builder.Services.AddSingleton(authenticationSettings);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<BookingSystemDbContext>
     (options => options.UseSqlServer("Server=DESKTOP-2UA5DVQ;Database=DeskBookingSystemDb;Trusted_Connection=True;"));
@@ -40,15 +43,31 @@ builder.Services.AddScoped<IlocationService,LocationsService>();
 builder.Services.AddScoped<IDesksService, DesksService>();
 builder.Services.AddScoped<IReservationsService, ReservationsService>();
 builder.Services.AddScoped<IDateValidationService, DateValidationService>();
+//Repositories DI
+builder.Services.AddScoped<IDeskRepository, DeskRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ILocationRepository, LocationRepository>();
+builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
+
+
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ExceptionHandlingMiddleware>();
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 builder.Services.AddScoped<IValidator<NewUserDto>, NewUserDtoValidator>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(setup =>
+    {
+        setup.AddPolicy("ui", builder =>
+            {
+                builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+
+            });
+    });
 
 var app = builder.Build();
 
+app.UseCors("ui");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
